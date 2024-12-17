e31bc85c80f476c076048429bc309c4d0bfb24f8
leopoldohuacasiv <125699543+leopoldohuacasiv@users.noreply.github.com>
2024-12-17 13:29:05 -0500

    Update i18n.js

--- frontend/src/translate/i18n.js
+++ frontend/src/translate/i18n.js
@@ -6,7 +6,7 @@ import { messages } from "./languages";
 i18n.use(LanguageDetector).init({
 	debug: false,
 	defaultNS: ["translations"],
-	fallbackLng: "es",
+	fallbackLng: "pt",
 	ns: ["translations"],
 	resources: messages,
 });
