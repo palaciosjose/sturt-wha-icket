2721fe00b442dee5a7f9b154e8e018005f0e7847
leopoldohuacasiv <leowin8@gmail.com>
2025-07-30 14:01:58 -0500

    🔧 Corregir URL de GitHub por defecto - Cambiar watoolx.git por watoolxoficial.git

--- install.sh
+++ install.sh
@@ -840,7 +840,7 @@ get_instancia_add() {
 get_link_git() {
     print_banner
     
-    default_link="https://github.com/leopoldohuacasiv/watoolx.git"
+    default_link="https://github.com/leopoldohuacasiv/watoolxoficial.git"
     printf "${WHITE} 💻 Ingresa el enlace de ${YELLOW}GITHUB${WHITE} de Watoolx que deseas instalar:${GRAY_LIGHT}\n"
     printf "  Presiona Enter para usar el valor por defecto:\n\n"
     printf "  ${default_link}\n\n"
